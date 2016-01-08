24803936e9326fd7a9cffdf5d68e08d22fc808bd
deepak khetwal <deepakkhetwal@gmail.com>
2016-01-08 16:36:30 -0500

    directive changes
    directive changes

--- jokes.main.component.js
+++ jokes.main.component.js
@@ -1,32 +1,35 @@
 /**
  * Created by dkhetwal on 1/7/2016.
  */
-angular
-    .module('shared.component',[])
-    .directive('loanLookup', jokesMain);
-jokesMain.$inject = ['jokesMainService'];
+(function () {
+    'use strict';
+    angular
+        .module('shared.component', [])
+        .directive('jokesMain', jokesMain);
+    JokesListController.$inject = ['jokesMainService'];
 
-function jokesMain(jokesMainService){
-    var directive = {
-        restrict: 'E',
-        templateUrl:'jokes-list.html',
-        controller: JokesListController,
-        controllerAs: 'vm'
-    };
-    return directive;
-}
-
-function JokesListController(){
-    var vm = this;
-    vm.dataLoaded = false;
-    vm.jokesList = [];
-    loadJokes();
-    function loadJokes(){
-        vm.dataLoaded = false;
-        jokesMainService.getJokeList
-            .query(function(data){
-                vm.jokesList = data;
-                vm.dataLoaded = true;
-            });
+    function jokesMain() {
+        var directive = {
+            restrict: 'E',
+            templateUrl: 'jokes-list.html',
+            controller: JokesListController,
+            controllerAs: 'vm'
+        };
+        return directive;
     }
-}
+
+    function JokesListController(jokesMainService) {
+        var vm = this;
+        vm.dataLoaded = false;
+        vm.jokesList = [];
+        loadJokes();
+        function loadJokes() {
+            vm.dataLoaded = false;
+            jokesMainService.getJokeList
+                .query(function (data) {
+                    vm.jokesList = data;
+                    vm.dataLoaded = true;
+                });
+        }
+    }
+})();
